0a6d33417d38736f01b1dc128774bbe6c71feee9
Dhruv Aeron <dhruv.aeron3@gmail.com>
2023-03-14 20:55:18 +0530

    dashboard page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Button } from 'react-bootstrap';
 import EntryPage from './page/entry/EntryPage';
 import Defaultlayout from './layout/Defaultlayout';
+import DashboardPage from './page/dashboard/DashboardPage';
 
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
       {/*<EntryPage />
        */}
 
-      <Defaultlayout><div className="hello">edfsdds</div></Defaultlayout>
+      <Defaultlayout>
+        <DashboardPage />
+      </Defaultlayout>
     </div>
   );
 }
--- src/layout/Defaultlayout.js
+++ src/layout/Defaultlayout.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Footer from './partials/Footer';
 import Header from './partials/Header';
 
-function Defaultlayout({childern}) {
+function Defaultlayout({ children }) {
   return (
     <div>
       <div className="default-layout">
         <div className="header">
           <Header />
         </div>
-        <main className="main">{childern}</main>
+        <main className="main">{children}</main>
       </div>
       <footer className="footer">
         <Footer />
